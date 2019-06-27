69854450154ff06f7fbee64e654c757aade8e1b1
Deepika Kripanithi <deepika.kripanithi@yahoo.in>
2019-06-27 23:09:59 +0530

    Add files via upload

---
+++ Table of Contents.mm
@@ -0,0 +1,250 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Table of Contents" FOLDED="false" ID="ID_1256018399" CREATED="1561637265420" MODIFIED="1561649841464" STYLE="oval"><hook NAME="MapStyle">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.connection" LAST="false">
+            <node_periodic_level_condition PERIOD="2" REMAINDER="1"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.topic" LAST="false">
+            <node_level_condition VALUE="2" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subtopic" LAST="false">
+            <node_level_condition VALUE="4" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subsubtopic" LAST="false">
+            <node_level_condition VALUE="6" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+    </conditional_styles>
+    <properties fit_to_viewport="false;" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="Arial" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork">
+<font NAME="Arial" SIZE="8" BOLD="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="1. Overview" POSITION="right" ID="ID_813562793" CREATED="1561647732705" MODIFIED="1561649841464" HGAP_QUANTITY="-2.499999508261695 pt" VSHIFT_QUANTITY="-59.999998211860714 pt">
+<node ID="ID_560030186" CREATED="1561647935665" MODIFIED="1561647935665"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1.1 Introduction
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_784184695" CREATED="1561647935670" MODIFIED="1561647935670"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1.2 Problem Statement
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_933774661" CREATED="1561647935675" MODIFIED="1561647935675"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1.3 Methods of Location Tracking
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1373067241" CREATED="1561647935679" MODIFIED="1561647935679"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;1.3.1 Outdoor Positioning Systems (OPS)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1551315251" CREATED="1561647935683" MODIFIED="1561647935683"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;1.3.2 Indoor Positioning Systems (IPS)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_282626716" CREATED="1561647935687" MODIFIED="1561647935687"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;1.3.3 Application Positioning Systems (APS)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1150649946" CREATED="1561647935691" MODIFIED="1561647935691"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1.4 Objectives of Study&#160;&#160;&#160;&#160;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_498536691" CREATED="1561647935696" MODIFIED="1561647935696"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1.5 Research Questions&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_245844719" CREATED="1561647935700" MODIFIED="1561647935700"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1.6 Outline of Dissertation
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="2, Literature Review" POSITION="left" ID="ID_1779560588" CREATED="1561648306644" MODIFIED="1561648323542">
+<node TEXT="2.1 Introduction" ID="ID_1462370810" CREATED="1561648333208" MODIFIED="1561648333208"/>
+<node TEXT="2.2 Literature Review on Outdoor Systems" FOLDED="true" ID="ID_608787863" CREATED="1561648333208" MODIFIED="1561648333208">
+<node TEXT="2.2.1 Literature on on Outdoor Positioning Systems (OPS)" ID="ID_1157777221" CREATED="1561648333209" MODIFIED="1561648333209"/>
+<node TEXT="2.2.1 Classification on various Outdoor Positioning Systems (OPS)" ID="ID_1629080401" CREATED="1561648333209" MODIFIED="1561648333209"/>
+</node>
+<node TEXT="2.2 Literature Review on Indoor Systems" FOLDED="true" ID="ID_1635429636" CREATED="1561648333209" MODIFIED="1561648333209">
+<node TEXT="2.2.1 Literature on on Indoor Positioning Systems (IPS)" ID="ID_1089699838" CREATED="1561648333209" MODIFIED="1561648333209"/>
+<node TEXT="2.2.1 Classification on various Indoor Positioning Systems (IPS)" ID="ID_799838617" CREATED="1561648333209" MODIFIED="1561648333209"/>
+</node>
+<node TEXT="2.3 Literature Review on Application Systems" FOLDED="true" ID="ID_802445858" CREATED="1561648333209" MODIFIED="1561648333209">
+<node TEXT="2.2.1 Literature on on Application Positioning Systems (APS)" ID="ID_1129815786" CREATED="1561648333209" MODIFIED="1561648333209"/>
+<node TEXT="2.2.1 Classification on various Application Positioning Systems (APS)" ID="ID_1552189111" CREATED="1561648333209" MODIFIED="1561648333209"/>
+</node>
+<node TEXT="2.4 Conclusion" ID="ID_1643359134" CREATED="1561648333209" MODIFIED="1561648333209"/>
+</node>
+<node TEXT="3. Outdoor Positioning Systems" POSITION="right" ID="ID_475065583" CREATED="1561649790148" MODIFIED="1561649825124" HGAP_QUANTITY="2.000000357627858 pt" VSHIFT_QUANTITY="51.74999845772986 pt">
+<node TEXT="3.1 Introduction" ID="ID_384912225" CREATED="1561649812609" MODIFIED="1561649812609"/>
+<node TEXT="3.2 Devices used in Outdoor Positioning Systems (OPS)" FOLDED="true" ID="ID_1531580811" CREATED="1561649812609" MODIFIED="1561649812609">
+<node TEXT="3.2.1 Adafruit FONA 808 - Mini Cellular GSM + GPS" FOLDED="true" ID="ID_1946205388" CREATED="1561649812611" MODIFIED="1561649812611">
+<node TEXT="3.2.1.1 Specifications, Features and Pin out" ID="ID_313599186" CREATED="1561649812611" MODIFIED="1561649812611"/>
+<node TEXT="3.2.1.2 Implementation and Results" ID="ID_327242412" CREATED="1561649812611" MODIFIED="1561649812611"/>
+<node TEXT="3.2.2 Pycom GPy Board + WiFi" FOLDED="true" ID="ID_1764619809" CREATED="1561649812611" MODIFIED="1561649812611">
+<node TEXT="3.2.2.1 Specifications, Features and Pin out" ID="ID_1754174791" CREATED="1561649812611" MODIFIED="1561649812611"/>
+<node TEXT="3.2.2.2 Implementation and Results" ID="ID_802875696" CREATED="1561649812611" MODIFIED="1561649812611"/>
+</node>
+<node TEXT="3.2.3 TK102B GPS GSM GPRS Tracker" FOLDED="true" ID="ID_15882804" CREATED="1561649812611" MODIFIED="1561649812611">
+<node TEXT="3.2.3.1 Specifications, Features and Pin out" ID="ID_328463620" CREATED="1561649812611" MODIFIED="1561649812611"/>
+<node TEXT="3.2.3.2 Implementation and Results" ID="ID_387979929" CREATED="1561649812611" MODIFIED="1561649812611"/>
+</node>
+<node TEXT="3.2.4. Quad band GPS Tracker Gt02A" FOLDED="true" ID="ID_1348153832" CREATED="1561649812611" MODIFIED="1561649812611">
+<node TEXT="3.2.3.1 Specifications, Features and Pin out" ID="ID_921155440" CREATED="1561649812611" MODIFIED="1561649812611"/>
+<node TEXT="3.2.3.2 Implementation and Results" ID="ID_1994437706" CREATED="1561649812611" MODIFIED="1561649812611"/>
+</node>
+<node TEXT="3.2.5 Tracking using Smartphone with Google" ID="ID_1590618144" CREATED="1561649812611" MODIFIED="1561649812611"/>
+<node TEXT="3.2.6 Tracking using Smartphone without Google" ID="ID_341950405" CREATED="1561649812611" MODIFIED="1561649812611"/>
+</node>
+</node>
+<node TEXT="3. Conclusion" ID="ID_1610737854" CREATED="1561649812611" MODIFIED="1561649812611"/>
+<node TEXT="3. Publication" ID="ID_1596276719" CREATED="1561649812612" MODIFIED="1561649812612"/>
+</node>
+</node>
+</map>
